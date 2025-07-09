bc485029691eee10aa70d3a35fb1a31e70742f81
Nader Nommen <109827474+NADN20@users.noreply.github.com>
2025-07-09 14:01:55 +0200

    third test

--- Section30 : classes/main.cpp
+++ Section30 : classes/main.cpp
@@ -3,10 +3,10 @@
 int main() {
 
 std::cout<<"Hello its just a test "<<std::endl ; 
-
-
-
 std::cout<<"Hello its just a second test "<<std::endl ; 
+std::cout<<"Hello its just a third  test "<<std::endl ; 
+
+
 
 return 0 ; 
     
