2147a791746c511c25b4faa6ae76c6e107fb7934
Nader Nommen <109827474+NADN20@users.noreply.github.com>
2025-07-09 12:29:26 +0000

    Ajout de main.cpp

--- Section30 : classes/main.cpp
+++ Section30 : classes/main.cpp
@@ -6,8 +6,6 @@ std::cout<<"Hello its just a test "<<std::endl ;
 std::cout<<"Hello its just a second test "<<std::endl ; 
 std::cout<<"Hello its just a third  test "<<std::endl ; 
 
-
-
 return 0 ; 
     
 }
