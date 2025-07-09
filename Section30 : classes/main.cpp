9deaaf8496dd6a1fce1283b974637dc0b4efc79b
Nader Nommen <109827474+NADN20@users.noreply.github.com>
2025-07-09 12:42:11 +0000

    ajout de test.h

--- Section30 : classes/main.cpp
+++ Section30 : classes/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream> 
-
+#include "test.h"
 int main() {
 
 std::cout<<"Hello its just a test "<<std::endl ; 
 std::cout<<"Hello its just a second test "<<std::endl ; 
 std::cout<<"Hello its just a third  test "<<std::endl ; 
-
+hello();
 return 0 ; 
     
 }
