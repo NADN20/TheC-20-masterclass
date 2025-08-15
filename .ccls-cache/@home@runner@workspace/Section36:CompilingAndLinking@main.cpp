048c64904e6e94075624bea3357455e4ae6e0588
Nader Noomen <46277258-nadernoomen@users.noreply.replit.com>
2025-08-15 14:58:19 +0000

    new commit

---
+++ .ccls-cache/@home@runner@workspace/Section36:CompilingAndLinking@main.cpp
@@ -0,0 +1,11 @@
+#include "utilities.h"   // is included in main.cpp
+#include  <iostream> 
+int main() {
+
+  double result = add(10,5);
+
+std::cout<<"Result is : "<<result<<std::endl; 
+  
+}
+
+
---
+++ .ccls-cache/@home@runner@workspace/Section36:CompilingAndLinking@utilities.h
@@ -0,0 +1,12 @@
+
+
+
+double add(double a, double b)  ; 
+
+
+double subtract(double a, double b) ;  
+
+
+double multiply(double a, double b) ;
+
+
